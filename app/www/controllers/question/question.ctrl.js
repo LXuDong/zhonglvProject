94f5a15a205131b96b79cbdc26b78d4ecf4381ec
LXuDong <372084672@qq.com>
2017-03-01 14:29:56 +0800

    lll

--- app/www/controllers/question/question.ctrl.js
+++ app/www/controllers/question/question.ctrl.js
@@ -1,3 +1,10 @@
-angular.module( "lotour.app").controller("questionCtrl",function( $scope,$http ){
+angular.module( "lotour.app").controller("questionCtrl",function( $scope,$http,httpServer ){
 
+	httpServer.get("Question/GetQuestionCategory",{
+		user_id:2
+	},function(data){
+		console.log(data)
+	},function(err){
+		console.log(err)
+	})
 })
